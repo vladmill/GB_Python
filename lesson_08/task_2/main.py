d3748907b92e6041a12c8cc3d469523f7094853b
vladmill <vladmill@mail.ru>
2023-02-26 13:54:54 +0700

    add hw_08

---
+++ lesson_08/task_2/main.py
@@ -0,0 +1,76 @@
+"""
+2. Задание на закрепление знаний по модулю json. Есть файл orders
+в формате JSON с информацией о заказах. Написать скрипт, автоматизирующий
+его заполнение данными.
+
+Для этого:
+Создать функцию write_order_to_json(), в которую передается
+5 параметров — товар (item), количество (quantity), цена (price),
+покупатель (buyer), дата (date). Функция должна предусматривать запись
+данных в виде словаря в файл orders.json. При записи данных указать
+величину отступа в 4 пробельных символа;
+Проверить работу программы через вызов функции write_order_to_json()
+с передачей в нее значений каждого параметра.
+
+ПРОШУ ВАС НЕ УДАЛЯТЬ ИСХОДНЫЙ JSON-ФАЙЛ
+ПРИМЕР ТОГО, ЧТО ДОЛЖНО ПОЛУЧИТЬСЯ
+
+{
+    "orders": [
+        {
+            "item": "принтер",
+            "quantity": "10",
+            "price": "6700",
+            "buyer": "Ivanov I.I.",
+            "date": "24.09.2017"
+        },
+        {
+            "item": "scaner",
+            "quantity": "20",
+            "price": "10000",
+            "buyer": "Petrov P.P.",
+            "date": "11.01.2018"
+        },
+        {
+            "item": "scaner",
+            "quantity": "20",
+            "price": "10000",
+            "buyer": "Petrov P.P.",
+            "date": "11.01.2018"
+        },
+        {
+            "item": "scaner",
+            "quantity": "20",
+            "price": "10000",
+            "buyer": "Petrov P.P.",
+            "date": "11.01.2018"
+        }
+    ]
+}
+
+вам нужно подгрузить JSON-объект
+и достучаться до списка, который и нужно пополнять
+а потом сохранять все в файл
+"""
+
+import json
+
+
+def write_order_to_json(item, quantity, price, buyer, date):
+    file = "orders.json"
+
+    with open(file, encoding="utf-8") as obj:
+        data = json.load(obj)
+        data['orders'].append(
+            {'item': item, 'quantity': quantity, 'price': price,
+             'buyer': buyer, 'date': date})
+
+    with open(file, "w", encoding="utf-8") as obj:
+        json.dump(data, obj, indent=4)
+
+    print(f'Данные добавлены в {file}')
+
+
+if __name__ == '__main__':
+    write_order_to_json("printer", "10", "6700", "Ivanov I.I.", "24.09.2017")
+    write_order_to_json("scaner", "20", "10000", "Petrov P.P.", "11.01.2018")
